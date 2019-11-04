2de17e32ebe1bb7d49f8c1b86cfdf5c5ad932186
Rakiyah Faatuono <rakiyahfaatuono@gmail.com>
2019-11-03 23:35:28 -0600

    ADDED METHOD IN CONTROLLER TO CREATE FORM TO ADD RESTAURANT

--- P4-Project_Setup/AllergenAlertMVC2/AllergenAlertMVC2/Controllers/RestaurantController.cs
+++ P4-Project_Setup/AllergenAlertMVC2/AllergenAlertMVC2/Controllers/RestaurantController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,6 +19,12 @@ namespace AllergenAlertMVC2.Controllers
 
             ViewBag.restaurants = restaurants;
 
+            return View();
+        }
+
+        //Method to create a form to add a restaurant to a list of restaurants
+        public IActionResult AddRestaurant()
+        {
             return View();
         }
     }
