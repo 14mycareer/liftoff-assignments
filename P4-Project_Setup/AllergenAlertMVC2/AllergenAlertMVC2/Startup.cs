f754148c46a80003d8bd893e69e897f1192de306
Rakiyah Faatuono <rakiyahfaatuono@gmail.com>
2019-11-04 01:02:07 -0600

    changed default method to restaurant controller in startup.cs

--- P4-Project_Setup/AllergenAlertMVC2/AllergenAlertMVC2/Startup.cs
+++ P4-Project_Setup/AllergenAlertMVC2/AllergenAlertMVC2/Startup.cs
@@ -43,7 +43,7 @@ namespace AllergenAlertMVC2
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Restaurant}/{action=Index}/{id?}");
             });
         }
     }
